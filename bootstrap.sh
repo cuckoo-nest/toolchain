6263a1f486e4c4f12dc9fb8a8dcb7e99c564fa64
Daniel <790119+DanTheMan827@users.noreply.github.com>
2025-11-13 13:22:06 -0600

    Use arm-2008q3-72-arm-none-linux-gnueabi-i686-pc-linux-gnu
    Target: arm-none-linux-gnueabi
    Configured with: /scratch/julian/lite-respin/linux/src/gcc-4.3/configure --build=i686-pc-linux-gnu --host=i686-pc-linux-gnu --target=arm-none-linux-gnueabi --enable-threads --disable-libmudflap --disable-libssp --disable-libstdcxx-pch --with-gnu-as --with-gnu-ld --enable-languages=c,c++ --enable-shared --enable-symvers=gnu --enable-__cxa_atexit --with-pkgversion='Sourcery G++ Lite 2008q3-72' --with-bugurl=https://support.codesourcery.com/GNUToolchain/ --disable-nls --prefix=/opt/codesourcery --with-sysroot=/opt/codesourcery/arm-none-linux-gnueabi/libc --with-build-sysroot=/scratch/julian/lite-respin/linux/install/arm-none-linux-gnueabi/libc --with-gmp=/scratch/julian/lite-respin/linux/obj/host-libs-2008q3-72-arm-none-linux-gnueabi-i686-pc-linux-gnu/usr --with-mpfr=/scratch/julian/lite-respin/linux/obj/host-libs-2008q3-72-arm-none-linux-gnueabi-i686-pc-linux-gnu/usr --disable-libgomp --enable-poison-system-directories --with-build-time-tools=/scratch/julian/lite-respin/linux/install/arm-none-linux-gnueabi/bin --with-build-time-tools=/scratch/julian/lite-respin/linux/install/arm-none-linux-gnueabi/bin
    Thread model: posix
    gcc version 4.3.2 (Sourcery G++ Lite 2008q3-72)

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 15f0f884-3bef-4524-8203-d56390034b55() {
+    local TOOLCHAIN_URL="https://github.com/cuckoo-nest/toolchain/releases/download/toolchains/arm-2008q3-72-arm-none-linux-gnueabi-i686-pc-linux-gnu.tar.bz2"
     local SCRIPT_DIR="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
 
     # Only fetch/extract if the file does not exist
-    if [[ ! -e "$SCRIPT_DIR/bin/arm-linux-gnueabi-gcc" ]]; then
+    if [[ ! -e "$SCRIPT_DIR/bin/arm-none-linux-gnueabi-gcc" ]]; then
         OUTPUT=$(
-            wget "$(cat "$SCRIPT_DIR/toolchain.txt")" -qO- 2>&1 | \
-            tar -xJvf - -C "$SCRIPT_DIR" --strip-components=1 2>&1
+            wget "$TOOLCHAIN_URL" -qO- 2>&1 | \
+            tar -xjvf - -C "$SCRIPT_DIR" --strip-components=1 2>&1
         )
         EXIT_CODE=$?
         
@@ -20,9 +21,12 @@
     echo "$SCRIPT_DIR/bin"
 }
 
-if [[ ! -n "$_5735245e1d5c433a661f2084a6657db18b3ec033" ]]; then
-    export _5735245e1d5c433a661f2084a6657db18b3ec033="$(15f0f884-3bef-4524-8203-d56390034b55)"
+if [[ -n "$_5735245e1d5c433a661f2084a6657db18b3ec033" ]]; then
+    export PATH="$_5735245e1d5c433a661f2084a6657db18b3ec033:$PATH"
+    echo "$_5735245e1d5c433a661f2084a6657db18b3ec033"
+else
+    export PATH="$(15f0f884-3bef-4524-8203-d56390034b55):$PATH"
+    15f0f884-3bef-4524-8203-d56390034b55
 fi
 
-export PATH="$_5735245e1d5c433a661f2084a6657db18b3ec033:$PATH"
-echo "$_5735245e1d5c433a661f2084a6657db18b3ec033"
+TOOLCHAIN_CROSS="arm-none-linux-gnueabi"
